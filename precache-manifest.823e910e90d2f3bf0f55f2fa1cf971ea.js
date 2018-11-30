9f1d3fe37948d291004c6f0e599702ee986409b5
Mounira94 <mounira.sghari94@gmail.com>
2018-11-30 10:58:44 +0100

    Updates

---
+++ precache-manifest.823e910e90d2f3bf0f55f2fa1cf971ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d9b57926b7846c790345",
+    "url": "/static/js/main.d9b57926.chunk.js"
+  },
+  {
+    "revision": "6b2fbd05086f6e302d25",
+    "url": "/static/js/1.6b2fbd05.chunk.js"
+  },
+  {
+    "revision": "d9b57926b7846c790345",
+    "url": "/static/css/main.a743919e.chunk.css"
+  },
+  {
+    "revision": "c396f67329517d78cdf90e7eb355a884",
+    "url": "/index.html"
+  }
+];
